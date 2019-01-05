cd3eaa1ecac6d28b1b02ba7fa35eb77dc1489e50
Ruzsa Balazs <cellux@omkamra.hu>
2019-01-05 20:16:22 +0100

    words like 123abc shall parse as symbols

--- sexp.lua
+++ sexp.lua
@@ -2,7 +2,6 @@ local util = require('util')
 local stream = require('stream')
 local buffer = require('buffer')
 local re = require('re')
-local adt = require('adt')
 
 local M = {}
 
@@ -35,9 +34,9 @@ local string_escapes = {
 }
 
 local regex = {
-   int = re.compile("^-?[0-9]+"),
-   float_suffix = re.compile("^(\\.[0-9]+)?(e-?[0-9]+)?"),
-   symbol = re.compile("^[^\\s)]+"),
+   ws = re.compile("^\\s+"),
+   word = re.compile("^\\S+"),
+   number = re.compile("^-?[0-9]+(\\.[0-9]+)?(e-?[0-9]+)?$"),
    comment = re.compile("^;.*?\\R"),
 }
 
@@ -45,7 +44,30 @@ M.Reader = function(input)
    local self = {}
    input = stream(input)
 
-   local function read_string()
+   local m
+   local function pmatch(pattern)
+      m = input:match(pattern)
+      return m
+   end
+
+   local function match(prefix)
+      if input:peek(#prefix) == prefix then
+         input:read(#prefix)
+         return true
+      else
+         return false
+      end
+   end
+
+   local function skip_ws()
+      input:match(regex["ws"])
+   end
+
+   local read_string
+   local read_list
+   local read_list_item
+
+   read_string = function()
       local str = buffer.new()
       while not input:eof() do
          local ch = input:read_char()
@@ -56,8 +78,7 @@ M.Reader = function(input)
                if replacement then
                   str:append(replacement)
                elseif ch == 'x' then
-                  local m = input:match("^[0-9a-fA-F]{2}")
-                  if m then
+                  if pmatch("^[0-9a-fA-F]{2}") then
                      str:append(string.char(tonumber(m[0], 16)))
                   else
                      str:append('\\x')
@@ -70,7 +91,7 @@ M.Reader = function(input)
                str:append('\\')
             end
          elseif ch == '"' then
-            return { "string", tostring(str) }
+            return String(tostring(str))
          else
             str:append(ch)
          end
@@ -78,60 +99,55 @@ M.Reader = function(input)
       util.throw("read error", "missing delimiter at end of string")
    end
 
-   local function skip_ws()
-      input:match("^\\s+")
-   end
-
-   local function match(prefix)
-      if input:peek(#prefix) == prefix then
-         input:read(#prefix)
-         return true
-      else
-         return false
-      end
-   end
-
-   local function read_list()
+   read_list = function(kind, delimiter)
       local items = {}
       while true do
-         skip_ws()
-         if match(')') then
-            break
-         end
-         local item = self:read()
+         local item = read_list_item(delimiter)
          if not item then
-            ef("missing end delimiter: %s", ')')
+            break
          else
             table.insert(items, item)
          end
       end
-      return { "list", items }
+      return { kind, items }
    end
 
-   function self:read()
+   read_list_item = function(delimiter)
       skip_ws()
-      local m
-      local function pmatch(pattern)
-         m = input:match(pattern)
-         return m
-      end
-      if pmatch(regex["int"]) then
-         local num = m[0]
-         if pmatch(regex["float_suffix"]) then
-            num = num .. m[0]
-            return Float(tonumber(num))
-         else
-            return Int(tonumber(num))
-         end
+      if delimiter and match(delimiter) then
+         return nil
       elseif match('"') then
          return read_string()
       elseif match('(') then
-         return read_list()
+         return read_list("list", ")")
       elseif pmatch(regex["comment"]) then
-         return self:read()
-      elseif pmatch(regex["symbol"]) then
-         return Symbol(m[0])
+         return read_list_item(delimiter)
+      elseif pmatch(regex["word"]) then
+         local word = m[0]
+         if delimiter then
+            local delim_index = word:find(delimiter, 1, true)
+            if delim_index then
+               input:unread(word:sub(delim_index))
+               word = word:sub(1,delim_index-1)
+            end
+         end
+         local m = regex["number"]:match(word)
+         if m then
+            if m[1] then
+               return Float(tonumber(m[0]))
+            else
+               return Int(tonumber(m[0]))
+            end
+         else
+            return Symbol(word)
+         end
+      elseif delimiter then
+         ef("missing end delimiter: %s", delimiter)
       end
+   end
+
+   function self:read()
+      return read_list_item()
    end
 
    return self
